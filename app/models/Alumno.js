69089e25d0fb0e0e38ba5f98c16a9b932507dd76
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-07-22 13:05:24 -0400

    Update Alumno.js

--- app/models/Alumno.js
+++ app/models/Alumno.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
-//MODELOS
-
 let tableName = "alumnos";
-
 let schema = mongoose.Schema;
-let Schema_ = new schema({
+let schema_ = new schema({
     nombre: String,
     edad: Number,
     profesion: String,
     id_Usuario: String,
 })
-let alumno = mongoose.model(tableName, Schema_);
-
-module.exports=alumno;
+let alumnos = mongoose.model(tableName, schema_);
+module.exports = alumnos;
