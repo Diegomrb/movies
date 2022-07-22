2b955d267b137c6c4d896120081abbe4f9312e3c
Weeks2 <106888385+Weeks2@users.noreply.github.com>
2022-07-22 13:13:20 -0400

    Update Usuario.js

--- app/models/Usuario.js
+++ app/models/Usuario.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
-let tableName = "usuario";
 let schema = mongoose.Schema;
 let schema_ = new schema({
     user: String,
     pass: String
 })
-let usuario = mongoose.model(tableName, schema_)
+let usuario = mongoose.model("usuario", schema_)
 module.exports=usuario;
