471f16df71fcd3b59ec5e143754c98ebcae1c5f4
Christian Ibarra <hola@weeks2.com>
2022-07-23 02:29:32 -0500

    (R) ui v2.

--- app/public/app.js
+++ app/public/app.js
@@ -105,9 +105,9 @@ function mostrarcontent(_content)
     {
         // ${element._id}
         divDatos.innerHTML += `<article class="col-md-12 mt-4 alert alert-secondary mx-1" style="background-color: white;">
-            <h6 data-id="${element._id}"> <b> ${element.title}</b> ${element.year}  ${element.genero}</h6>
-            <input data-idModificar="${element._id}" type="button" class="modificar form-select-sm" value="Modificar">
-            <input data-idEliminar="${element._id}" type="button" class="eliminar form-select-sm" value="Eliminar">
+            <h6 data-id="${element._id}"> <b><h4> ${element.title}</h4></b> ${element.year}  ${element.genero}</h6>
+            <input data-idModificar="${element._id}" type="button" class="modificar form-select-sm float-end btn btn-warning" value="Modificar">
+            <input data-idEliminar="${element._id}" type="button" class="eliminar form-select-sm float-end btn btn-danger" value="Eliminar">
             </article>`
     });
 
